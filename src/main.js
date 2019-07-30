489e6444e6d306c866bf374d88625626d35aacae
Curt Mullin <mullinc@oya.state.or.us>
2019-07-30 14:57:27 -0700

    wip

---
+++ src/main.js
@@ -0,0 +1,20 @@
+// article originally at https://dev.to/ycmjason/building-a-simple-virtual-dom-from-scratch-3d05#background-what-is-virtual-dom
+import createElement from './createElement';
+import render from './render';
+
+const vApp = createElement('div', {
+  attrs: {
+    id: 'app',
+    children: [
+      'Hello!',
+      createElement('img', {
+        attrs: {
+          src: 'https://media.giphy.com/media/cuPm4p4pClZVC/giphy.gif'
+        }
+      })
+    ]
+  },
+});
+
+const $app = render(vApp);
+console.log(vApp);
